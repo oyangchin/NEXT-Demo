17bd20c92e316a027223855c48b2850bf3b78753
Joy <chin.oyang@z-tron.net>
2019-04-08 17:06:31 +0800

    add header component

--- 00.01. getting start and Link/pages/index.js
+++ 00.01. getting start and Link/pages/index.js
@@ -1,14 +1,12 @@
 
-// This is the Link API
-import Link from 'next/link'
+import Header from '../components/header';
 
 const Index = () => (
   <div>
-    <Link href="/about">
-      <button>About Page</button>
-    </Link>
+    <Header />
     <p>Hello Next.js</p>
   </div>
 )
 
 export default Index
+
