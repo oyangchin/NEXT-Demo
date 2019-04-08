a978b1b08637ec23f763c60602baa84e49f1c2c3
Joy <chin.oyang@z-tron.net>
2019-04-08 16:04:19 +0800

    Using Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+
+// This is the Link API
+import Link from 'next/link'
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
