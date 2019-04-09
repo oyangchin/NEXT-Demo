7ec1fadc4f469c5357448b1d93bd8f0e26057011
Joy <chin.oyang@z-tron.net>
2019-04-09 10:45:17 +0800

    ctrl+C stop the app

---
+++ 00.01. getting start and Link/.next/static/webpack/static/development/pages/index.js.f0731ee4103b5d364a0a.hot-update.js
@@ -0,0 +1,54 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/MyLayout.js":
+/*!********************************!*\
+  !*** ./components/MyLayout.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Layout; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Header */ "./components/Header.js");
+var _jsxFileName = "/Users/oyangchin/Documents/practice/Nextjs/NEXT-Demo/00.01. getting start and Link/components/MyLayout.js";
+
+// components/MyLayout.js
+
+var layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD' // const Layout = props => {
+  //   return () => (
+  //     <div style={layoutStyle}>
+  //       <Header />
+  //       {props.children}
+  //     </div>
+  //   )
+  // }
+  // export default Layout
+
+};
+function Layout(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    style: layoutStyle,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Header__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: this
+  }), props.children);
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.f0731ee4103b5d364a0a.hot-update.js.map
