9aa924458cc7a075f19aab175fa83c515ba33c60
Joy <chin.oyang@z-tron.net>
2019-04-09 10:39:54 +0800

    Clean URLs with Route Masking

--- 00.01. getting start and Link/pages/index.js
+++ 00.01. getting start and Link/pages/index.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -17,9 +17,9 @@ export default function Blog() {
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title="Hello Next.js"></PostLink>
-        <PostLink title="Learn Next.js is awesome"></PostLink>
-        <PostLink title="Deploy apps with Zeit"></PostLink>
+        <PostLink id="hello-nextjs"  title="Hello Next.js"></PostLink>
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome"></PostLink>
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"></PostLink>
       </ul>
     </Layout>
   )
