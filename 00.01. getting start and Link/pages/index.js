82034ef262943cae38e21b7f2d878c874fd610c5
Joy <chin.oyang@z-tron.net>
2019-04-09 10:03:35 +0800

    Adding a list of posts

--- 00.01. getting start and Link/pages/index.js
+++ 00.01. getting start and Link/pages/index.js
@@ -1,6 +1,26 @@
 
-import withLayout from '../components/MyLayout'
+import Layout from '../components/MyLayout'
+import Link from 'next/link'
 
-const Page = () => <p>Hello Next.js</p>
+// const Page = () => <p>Hello Next.js</p>
 
-export default withLayout(Page)
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js"></PostLink>
+        <PostLink title="Learn Next.js is awesome"></PostLink>
+        <PostLink title="Deploy apps with Zeit"></PostLink>
+      </ul>
+    </Layout>
+  )
+}
